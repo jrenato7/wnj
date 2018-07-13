5c7a689e222d8b36deb2f3caa1fbaa21645bd349
Jose Renato <jose.renato77@gmail.com>
2018-07-12 23:22:53 -0300

    Correct permissions for admin users

--- wnj/accounts/admin.py
+++ wnj/accounts/admin.py
@@ -14,22 +14,22 @@ class UserAdmin(BaseUserAdmin):
     # The fields to be used in displaying the User model.
     # These override the definitions on the base UserAdmin
     # that reference specific fields on auth.User.
-    list_display = ('email', 'is_admin', 'created_at')
-    list_filter = ('is_admin',)
+    list_display = ('email', 'is_admin', 'staff', 'created_at')
+    list_filter = ('is_admin', 'staff')
     fieldsets = (
         (None, {'fields': ('email', 'password')}),
-        ('Permissions', {'fields': ('is_admin',)}),
+        ('Permissions', {'fields': ('is_admin','staff')}),
     )
     # add_fieldsets is not a standard ModelAdmin attribute. UserAdmin
     # overrides get_fieldsets to use this attribute when creating a user.
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('email', 'password1', 'password2')}
+            'fields': ('first_name','email', 'password1', 'password2')}
         ),
     )
     search_fields = ('email',)
-    ordering = ('email',)
+    ordering = ('created_at',)
     filter_horizontal = ()
 
 # Now register the new UserAdmin...
